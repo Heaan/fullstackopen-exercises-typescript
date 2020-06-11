a954722e824ae15b3bc0fdb09f3d2e06b6b75ceb
Heaan <cxy.eml@live.cn>
2020-06-11 16:37:41 +0800

    add part9 exercise 9.10

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 
+import diagnoseRouter from './routers/diagnoses';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,6 +13,8 @@ app.get('/api/ping', (_req, res) => {
   console.info('someone pinged here');
   res.send('pong!');
 });
+
+app.use('/api', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.info(`Server running on port ${PORT}`);
